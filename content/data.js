81f9f7f902c18cbd9dfe822337901f0da42fd036
Jujhaar <jujhaar.singh.123@gmail.com>
2020-05-05 07:44:07 +0530

    removing tsnocheck from files in content folder

--- content/data.js
+++ content/data.js
@@ -1,42 +1,41 @@
-// @ts-nocheck
 export const skills = [
   {
-    title: "Languages",
-    list: ["Python", "Javascript", "HTML", "CSS"],
+    title: 'Languages',
+    list: ['Python', 'Javascript', 'HTML', 'CSS'],
   },
   {
-    title: "Frontend Web Frameworks",
-    list: ["Reactjs", "Nextj"],
+    title: 'Frontend Web Frameworks',
+    list: ['Reactjs', 'Nextj'],
   },
   {
-    title: "Backend Web Frameworks",
-    list: ["ExpressJs", "Flask"],
+    title: 'Backend Web Frameworks',
+    list: ['ExpressJs', 'Flask'],
   },
   {
-    title: "Python Libraries",
-    list: ["Flask", "Matplotlib", "Tkinter", "Numpy"],
+    title: 'Python Libraries',
+    list: ['Flask', 'Matplotlib', 'Tkinter', 'Numpy'],
   },
   {
-    title: "Other Skills",
+    title: 'Other Skills',
     list: [
-      "Basics of VueJs",
-      "Basics of AngularJs",
-      "Basics of Java",
-      "Basics of Kotlin",
-      "Basics of Go",
+      'Basics of VueJs',
+      'Basics of AngularJs',
+      'Basics of Java',
+      'Basics of Kotlin',
+      'Basics of Go',
     ],
   },
 ];
 
 export const projects = [
   {
-    name: "FlaPPPPPy Bird Game",
+    name: 'FlaPPPPPy Bird Game',
     description:
-      "The legendary game flappy bird recreated with p5js(notice the name!) library in the browser.",
-    liveLocation: "https://jujhaar2409.github.io/flaPPPPPy-bird/",
-    codeLocation: "https://github.com/jujhaar2409/flaPPPPPy-bird",
+      'The legendary game flappy bird recreated with p5js(notice the name!) library in the browser.',
+    liveLocation: 'https://jujhaar2409.github.io/flaPPPPPy-bird/',
+    codeLocation: 'https://github.com/jujhaar2409/flaPPPPPy-bird',
   },
 ];
 
 export const index =
-  "Hey I am an aspiring Computer Engineer currently in 12th Grade and soon to go to college. I found that I enjoyed creating things with my coding skills. I can now create interactive, mobile first websites which are interactive and engaging. To learn more about me click the button below!";
+  'Hey I am an aspiring Computer Engineer currently in 12th Grade and soon to go to college. I found that I enjoyed creating things with my coding skills. I can now create interactive, mobile first websites which are interactive and engaging. To learn more about me click the button below!';
